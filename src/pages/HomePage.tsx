40da59de8bffa366d948636fda99aebc85b8a0bf
CxO19 <mateoortegasaca@gmail.com>
2026-07-07 08:54:44 -0500

    feat: Included author name

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div>
       <p style={{ color: '#6b7280' }}>
-        Bienvenido a Una app con Router
+        Autor: Mateo Ortega
       </p>
     </div>
   )
